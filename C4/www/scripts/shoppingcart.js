12b33c9e55a11a69f47f72b20d2251ee9933e4b6
Liang Xin <liangxin4821@gmail.com>
2019-11-25 21:10:18 +0800

    1
    1

--- C4/www/scripts/shoppingcart.js
+++ C4/www/scripts/shoppingcart.js
@@ -1,74 +1,131 @@
-﻿//Press Show all to see products
+﻿var sum = 0
 
 $(document).ready(function () {
 
-
-
-    //Activiating this fuction;
     GET_ShoppingCart();
 
-    //Creating a function that get product basic details from the database
     function GET_ShoppingCart() {
         var useremail = localStorage.getItem("customer_email");
         var url = "https://bitmp08.projectsbit.org/MobileApp/getshoppingcart.php";
-        // this  the start of a ajax function (a fuction use to call to php for database query)
+
         $.ajax({
-            // 1. set the destination for this package of content
+
             url: url,
-            //2. the ways use to sent the package
             method: 'POST',
-            //3. Give the package a name for the PHP side to open the correct package
             data: { GET_ShoppingCart: 1 },
-            //4. Break open the response received 
             success: function (response) {
                 console.log(response);
                 var resp = jQuery.parseJSON(JSON.stringify(response));
-
-
-
                 var responselength = resp.message.length;
 
-
-                if (resp.status === 1) {
-                    //creating a variable to store the final display conetent          
+                if (resp.status === 1) {         
                     var ProductContent = '';
-                    ProductContent +=
-
-
-                        ' <h1>ShoppingCart</h1>'
-                        + ' <h1>productID</h1>'
-
-
-                        ;
-
+                  
                     $.each(resp.message, function (index, value) {
-
                         if (useremail === value.customer_email) {
+                            ProductContent +=                              
+                                '<div class="cart-row" >'+
+                                    '<div class="cart-item cart-column">'+
+                                        '<img class="cart-item-image" src="https://bitmp08.projectsbit.org/product_images/' + value.product_picture + '" width="100" height="100">' +
+                                        '<span class="cart-item-title">' + value.product_name +'</span>'+
+                                    '</div>'+
 
-                            ProductContent +=
-                           '<img src="https://bitmp08.projectsbit.org/product_images/' + value.product_picture + '"style="width:100px; height:100px;"/>'+
-                           ' <input type="checkbox" name="vehicle1" value="Bike"> ' + value.product_id + ',' + value.product_name + ',' + '$' + value.product_price + '<br><hr>';
+                                      '<span class="cart-price cart-column">$' + value.product_price +'</span>'+
+
+                                        '<div class="cart-quantity cart-column">'+
+                                       '<input id="' + value.product_id + '" class="cart-quantity-input" type="number" value="1">' +
+                                         '<button id="' + value.product_id + '" class="btn btn-danger" type="button">REMOVE</button>'+
+                                         '</div>'+
+                                '</div>';
+
+                             sum = sum + parseInt(value.product_price);
+                            document.getElementsByClassName('cart-total-price')[0].innerText = '$' + sum
                         }
-
-
-                    });
-                    ProductContent +=
-
-
-                   //' <button>Make Order</button>';
-
-
-                        
-
-                    $("#shoppingcart_list").html(ProductContent);
-
-
+                    });                    
+                    $(".cart-items").html(ProductContent);
                 }
             }
         });
     }
+    updateCartTotal()
+
+        
+    //Execute Remove
+    $(document.body).on('click', '.btn-danger', function () {
+
+            var buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.remove()
+            var productid = $(this).attr('id');
+            localStorage.setItem("productid", productid);
+            removefromcart();
+            updateCartTotal();
+        
+    }); 
+
+    //Change quantity
+    $(document.body).on('keyup', '.cart-quantity-input', function () {
+        var productid = $(this).attr('id');
+
+        if (isNaN($(this).val()) || $(this).val() <= 0 ) {
+            alert('Please enter a valid input');
+
+        }
+        updateCartTotal()
+    });
+
+
 
 });
+
+//Remove from Cart
+function removefromcart() {
+    var productid = localStorage.getItem("productid");
+
+
+    var url = serverURL() + "/removefromcart.php";
+
+    var JSONObject = {
+        "productid": productid,
+    };
+  
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: JSONObject,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (arr) {
+            //_changeAddressResult(arr);
+            alert('Removed');
+        },
+        error: function () {
+            alert("FAIL");
+        }
+    });
+}
+
+//Cart Total
+function updateCartTotal() {
+
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value;
+        total = total + (price * quantity)  
+    }
+    total = Math.round(total * 100) / 100  
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+
+
+}
+
 
 
 
