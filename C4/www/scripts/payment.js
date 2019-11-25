bf72d540cebe65f3e3e3b1966f670967f93026df
Liang Xin <liangxin4821@gmail.com>
2019-11-25 19:01:01 +0800

    1
    1

--- C4/www/scripts/payment.js
+++ C4/www/scripts/payment.js
@@ -1,73 +1,23 @@
-﻿
-paypal.Buttons({
+﻿var finalvalue = 100;
+function paypal(){
+    paypal.Buttons({
         createOrder: function(data, actions) {
-                    return actions.order.create({
-        purchase_units: [
-                            {
-
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
         amount: {
-        currency_code: "USD",
-        value: "170.00",
-        breakdown: {
-        item_total: {
-        currency_code: "USD",
-        value: "180.00"
-                            },
-        shipping: {
-        currency_code: "USD",
-        value: "00.00"
-                            },
-
-
-        shipping_discount: {
-        currency_code: "USD",
-          value: "10"
-                           }
-                           }
-                          },
-        items: [
-         {
-         name: "T-Shirt",
-         description: "Green XL",
-         sku: "sku01",
-         unit_amount: {
-        currency_code: "USD",
-         value: "90.00"
-                            },
-
-        quantity: "1",
-        category: "PHYSICAL_GOODS"
-                            },
-                            {
-        name: "Shoes",
-        description: "Running, Size 10.5",
-        sku: "sku02",
-        unit_amount: {
-        currency_code: "USD",
-        value: "45.00"
-                            },
-        quantity: "2",
-         category: "PHYSICAL_GOODS"
-                             }
-                             ]
-
-                            }
-                        ]
-                    });
-                },
-                onApprove: function(data, actions) {
-                    return actions.order.capture().then(function(details) {
+            value: finalvalue
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
         alert('Transaction completed by ' + details.payer.name.given_name);
-    // Call your server to save the transaction
-    return fetch('/api/paypal-transaction-complete', {
-        method: 'post',
-                            headers: {
-        'content-type': 'application/json'
-                            },
-                            body: JSON.stringify({
-        orderID: data.orderID
-                            })
-                        });
-                    });
-                }
-            }).render('#paypal-button-container');
+    });
+    }
+  }).render('#paypal-button-container');
+  //This function displays Smart Payment Buttons on your web page.
+}
