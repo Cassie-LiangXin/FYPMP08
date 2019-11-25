dd139afc7f31ae2ea110f67fecd8220d1859a138
Liang Xin <liangxin4821@gmail.com>
2019-11-25 17:35:04 +0800

    1
    1

--- C4/www/scripts/order.js
+++ C4/www/scripts/order.js
@@ -1,4 +1,4 @@
-﻿var scr = document.getElementsById("pp")[0]; 
-var value = 666.00;
+﻿//var scr = document.getElementsById("pp")[0]; 
+//var value = 666.00;
 // assuming that this <script> is first <script> on this page
 //scr.setAttribute("amowsunt", value);
